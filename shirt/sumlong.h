f0b8052faf73fb7b828b8f1c1aea9e700072ead6
tanachote <63021697@up.ac.th>
2022-03-11 12:18:21 +0700

    first commit

---
+++ shirt/sumlong.h
@@ -0,0 +1,249 @@
+<div class="img2">
+    <img src="เสื้อถักนิตติ้ง.jpg" alt="เสื้อถักนิตติ้ง" width="350" height="255">
+</div>
+<div class="text2">
+    <p style="font-size:24px;">เสื้อถักนิตติ้ง
+    <p>
+    <p style="font-size:18px;">99$
+    <p>
+</div>
+<div class="img3">
+    <img src="เสื้อถักสตรี.webp" alt="เสื้อถักสตรี" width="350" height="255">
+</div>
+<div class="text3">
+    <p style="font-size:24px;">เสื้อถักสตรี
+    <p>
+    <p style="font-size:18px;">99$
+    <p>
+</div>
+<div class="img4">
+    <img src="เสื้อไทลื้อ.jpg" alt="เสื้อไทลื้อ" width="350" height="255">
+</div>
+<div class="text4">
+    <p style="font-size:24px;">เสื้อไทลื้อ
+    <p>
+    <p style="font-size:18px;">99$
+    <p>
+</div>
+<div class="img5">
+    <img src="เสื้อผ้าพื้นเมือง.jpg" alt="เสื้อผ้าพื้นเมือง" width="350" height="255">
+</div>
+<div class="text5">
+    <p style="font-size:24px;">เสื้อผ้าพื้นเมือง
+    <p>
+    <p style="font-size:18px;">99$
+    <p>
+</div>
+<div class="img6">
+    <img src="เสื้อลายต่อดอก.jpg" alt="เสื้อลายต่อดอก" width="350" height="255">
+</div>
+<div class="text6">
+    <p style="font-size:24px;">เสื้อลายต่อดอก
+    <p>
+    <p style="font-size:18px;">99$
+    <p>
+</div>
+<div class="img7">
+    <img src="เสื้อสตรีผ้าปักชาวเขา.jpg" alt="เสื้อสตรีผ้าปักชาวเขา" width="350" height="255">
+</div>
+<div class="text7">
+    <p style="font-size:24px;">เสื้อสตรีผ้าปักชาวเขา
+    <p>
+    <p style="font-size:18px;">99$
+    <p>
+</div>
+<div class="img8">
+    <img src="เสื้อไทลื้อแปรรูปสีธรรมชาติ.jpg" alt="เสื้อไทลื้อแปรรูปสีธรรมชาติ" width="350" height="255">
+</div>
+<div class="text8">
+    <p style="font-size:24px;">เสื้อไทลื้อแปรรูปสีธรรมชาติ
+    <p>
+    <p style="font-size:18px;">99$
+    <p>
+</div>
+<div class="img9">
+    <img src="เสื้อลายสถานที่ท่องเที่ยว จ.พะเยา 01.gif" alt="เสื้อลายสถานที่ท่องเที่ยว" width="350" height="255">
+</div>
+<div class="text9">
+    <p style="font-size:24px;">เสื้อลายสถานที่ท่องเที่ยว จ.พะเยา
+    <p>
+    <p style="font-size:18px;">99$
+    <p>
+</div>
+<div class="img10">
+    <img src="เสื้อลายสถานที่ท่องเที่ยว จ.พะเยา 02.gif" alt="เสื้อลายสถานที่ท่องเที่ยว" width="350" height="255">
+</div>
+<div class="text10">
+    <p style="font-size:24px;">เสื้อลายสถานที่ท่องเที่ยว จ.พะเยา
+    <p>
+    <p style="font-size:18px;">99$
+    <p>
+</div>
+<div class="img11">
+    <img src="เสื้อลายสถานที่ท่องเที่ยว จ.พะเยา 03.gif" alt="เสื้อลายสถานที่ท่องเที่ยว" width="350" height="255">
+</div>
+<div class="text11">
+    <p style="font-size:24px;">เสื้อลายสถานที่ท่องเที่ยว จ.พะเยา
+    <p>
+    <p style="font-size:18px;">99$
+    <p>
+</div>
+<div class="img12">
+    <img src="เสื้อลายสถานที่ท่องเที่ยว จ.พะเยา 04.gif" alt="เสื้อลายสถานที่ท่องเที่ยว" width="350" height="255">
+</div>
+<div class="text12">
+    <p style="font-size:24px;">เสื้อลายสถานที่ท่องเที่ยว จ.พะเยา
+    <p>
+    <p style="font-size:18px;">99$
+    <p>
+</div>
+    </div>
+<div class="botton01">
+<button class="button button2"><</button>
+</div>
+<div class="footer">
+<p style="font-size:18px;">1
+<p>
+</div>
+<div class="botton02">
+<button class="button button3">></button>
+</div>
+.button {
+    border: none;
+    color: white;
+    padding: 5px 10px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 18px;
+    margin: 4px 2px;
+    cursor: pointer;
+    font-family: 'Sarabun', sans-serif;
+}
+.button2 {
+    background-color: #000000;
+    position: relative;
+    left: 670px;
+    bottom: 2950px
+}
+.button3 {
+    background-color: #000000;
+    position: relative;
+    left: 787px;
+    bottom: 3050px
+}
+.footer {
+    position: relative;
+    left: 740px;
+    bottom: 3000px;
+}
+.img2 {
+    position: relative;
+    left: 560px;
+    bottom: 374px;
+}
+.text2 {
+    position: relative;
+    left: 560px;
+    bottom: 383px;
+}
+.img3 {
+    position: relative;
+    left: 970px;
+    bottom: 740px;
+}
+.text3 {
+    position: relative;
+    left: 970px;
+    bottom: 760px;
+}
+.img4 {
+    position: relative;
+    left: 150px;
+    bottom: 740px;
+}
+.text4 {
+    position: relative;
+    left: 150px;
+    bottom: 760px;
+}
+.img5 {
+    position: relative;
+    left: 560px;
+    bottom: 1120px;
+}
+.text5 {
+    position: relative;
+    left: 560px;
+    bottom: 1137px;
+}
+.img6 {
+    position: relative;
+    left: 970px;
+    bottom: 1500px;
+}
+.text6 {
+    position: relative;
+    left: 970px;
+    bottom: 1515px;
+}
+.img7 {
+    position: relative;
+    left: 150px;
+    bottom: 1500px;
+}
+.text7 {
+    position: relative;
+    left: 150px;
+    bottom: 1515px;
+}
+.img8 {
+    position: relative;
+    left: 560px;
+    bottom: 1880px;
+}
+.text8 {
+    position: relative;
+    left: 560px;
+    bottom: 1890px;
+}
+.img9 {
+    position: relative;
+    left: 970px;
+    bottom: 2260px;
+}
+.text9 {
+    position: relative;
+    left: 970px;
+    bottom: 2270px;
+}
+.img10 {
+    position: relative;
+    left: 150px;
+    bottom: 2260px;
+}
+.text10 {
+    position: relative;
+    left: 150px;
+    bottom: 2270px;
+}
+.img11 {
+    position: relative;
+    left: 560px;
+    bottom: 2640px;
+}
+.text11 {
+    position: relative;
+    left: 560px;
+    bottom: 2650px;
+}
+.img12 {
+    position: relative;
+    left: 970px;
+    bottom: 3020px;
+}
+.text12 {
+    position: relative;
+    left: 970px;
+    bottom: 3030px;
+}
